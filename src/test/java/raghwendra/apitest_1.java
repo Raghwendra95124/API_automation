5a32d30f5d110aab9d4c9c134ee5c40334f9d669
raghwerndra95124 <raghwerndrasingh95124@gmail.com>
2025-09-21 15:47:30 +0530

    api_test1

--- src/test/java/raghwendra/apitest_1.java
+++ src/test/java/raghwendra/apitest_1.java
@@ -1,2 +1,11 @@
-package raghwendra;public class apitest_1 {
+package raghwendra;
+
+import io.restassured.RestAssured;
+
+public class apitest_1 {
+    public static void main(String[] args) {
+        System.out.println("love you shikha");
+
+
+    }
 }
